2a5cd1a00cb15eef1ac3160c393a1ad61f582a9c
shrutivanw <shruti.dalvi@gmail.com>
2019-01-29 19:10:42 -0500

    C11 updates
    - Ask for time and space complexity explanations in comments above each method.
    - Elaborate that space and time complexity defined in terms of n should explain what n stands for.

--- lib/using_restricted_array.rb
+++ lib/using_restricted_array.rb
@@ -6,51 +6,72 @@ require_relative 'restricted_array.rb'
 
 # Calculates the length of the restricted array. All values are integers.
 # The restricted_array is terminated by 'nil' i.e. array[length] = nil
+# Time complexity: ?
+# Space complexity: ?
 def length(array)
   raise NotImplementedError
 end
 
 # Prints each integer values in the array
+# Time complexity: ?
+# Space complexity: ?
 def print_array(array)
   raise NotImplementedError
 end
 
 # For an unsorted array, searches for 'value_to_find'.
 # Returns true if found, false otherwise.
+# Time complexity: ?
+# Space complexity: ?
 def search(array, length, value_to_find)
   raise NotImplementedError
 end
 
 # Finds and returns the largest integer value the array
 # Assumes that the array is not sorted.
+# Time complexity: ?
+# Space complexity: ?
 def find_largest(array, length)
   raise NotImplementedError
 end
 
 # Finds and returns the smallest integer value in the array
 # Assumes that the array is not sorted.
+# Time complexity: ?
+# Space complexity: ?
 def find_smallest(array, length)
   raise NotImplementedError
 end
 
 # Reverses the values in the integer array in place
+# Time complexity: ?
+# Space complexity: ?
 def reverse(array, length)
   raise NotImplementedError
 end
 
 # For an array sorted in ascending order, searches for 'value_to_find'.
 # Returns true if found, false otherwise.
+# Time complexity: ?
+# Space complexity: ?
 def binary_search(array, length, value_to_find)
   raise NotImplementedError
 end
 
 # Helper method provided to sort the array in ascending order
 # Implements selection sort
-# Time complexity = O(n^2) since to find the correct value to be in a given location,
-# all the remaining elements are visited. This is done for each location.
-# (nested loop of size n each)
-# Space complexity = O(1) since the additional storage needed does not depend
-#                    on input array size.
+# Time complexity = O(n^2), where n is the number of elements in the array.
+# To find the correct value for index 0, a total of (n-1) comparisons are needed.
+# To find the correct value for index 1, a total of (n-2) comparisons are needed.
+# To find the correct value for index 2, a total of (n-3) comparisons are needed.
+# and so on ...
+# To find the correct value for index (n-2), a total of 1 comparisons is needed.
+# To find the correct value for the last index, a total of 0 comparisons are needed.
+# Total number of comparisons = (n-1) + (n-2) + ... 3 + 2 + 1
+#                             = (n * (n-1))/2
+# This is O(n^2) in Big O terms.
+# Space complexity = constant or O(1) since the additional storage needed,
+# does not depend on input array size.
 def sort(array, length)
   length.times do |index| # outer loop - n elements
     min_index = index # assume index is where the next minimally value is
